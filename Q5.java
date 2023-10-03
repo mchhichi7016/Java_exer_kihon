1d0b708b5fd87f82c2690569709cb19475bc4975
MINAMINO <causewaybar711@gmail.com>
2023-10-03 22:49:59 +0900

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = 48;
+
+        System.out.println("変数aの値は" + a + "です。");
+        System.out.println("変数bの値は" + b + "です。");
+    }
+}
