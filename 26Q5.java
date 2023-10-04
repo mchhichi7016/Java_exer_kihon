6fbce0010c926afb8d4105c00cc0d8aee49d7c8e
MINAMINO <causewaybar711@gmail.com>
2023-10-04 20:51:03 +0900

    Create 26Q5.java

---
+++ 26Q5.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String value1 = "B";
+        double value2 = 3.14;
+
+        System.out.println(value1);
+        System.out.println(value2);
+
+
+
+    }
+}
+
