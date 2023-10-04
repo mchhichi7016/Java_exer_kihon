eb05204adc81abdf0d9bc633fad8fceef5aff64b
MINAMINO <causewaybar711@gmail.com>
2023-10-04 19:30:11 +0900

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b;
+
+        System.out.println(a);
+
+        b = a;
+
+        System.out.println(b);
+
+
+    }
+}
+
