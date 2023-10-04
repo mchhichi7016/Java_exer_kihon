b556e947f5471edaa0b010b09cbac03594661727
MINAMINO <causewaybar711@gmail.com>
2023-10-04 21:01:40 +0900

    Create 28Q1.java

---
+++ 28Q1.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(4 * 3);
+        System.out.println(15 % 4);
+
+        int a = 5;
+
+        System.out.println(a + 7);
+
+        a += 4;
+        System.out.println(a);
+
+    }
+}
+
