e5e7bda0de4e384ac8f6888841b5c191a426a9bf
MINAMINO <causewaybar711@gmail.com>
2023-10-04 21:26:11 +0900

    Create 32Q3.java

---
+++ 32Q3.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int a = 5 * 3;
+        int b = 10 / 3;
+        double c = (double) 10 / 3;
+        int d = a * b;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+
+    }
+}
+
