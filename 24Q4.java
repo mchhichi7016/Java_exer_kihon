c64af120726a5c5fc2e3cffc8ed02d358f2f07a6
MINAMINO <causewaybar711@gmail.com>
2023-10-04 20:46:54 +0900

    Create 24Q4.java

---
+++ 24Q4.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char a = 'A';
+        double b = 3.14;
+
+        String c;
+        float d;
+
+        c = a;
+        d = b;
+
+
+
+    }
+}
+
