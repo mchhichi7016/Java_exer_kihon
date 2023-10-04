7ca8da6225241fe861f02e1fd9122eb4c6ffc57c
MINAMINO <causewaybar711@gmail.com>
2023-10-04 19:33:30 +0900

    Create Q7.java

---
+++ Q7.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 88;
+        double d = 12.08;
+        char c = 'A';
+        String s = "I LIKE JAVA";
+
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(s);
+
+    }
+}
+
