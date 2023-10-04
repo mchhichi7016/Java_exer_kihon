11271c68f27e9f4b32df8d1e1390660fc74abc99
MINAMINO <causewaybar711@gmail.com>
2023-10-04 21:20:27 +0900

    Create 31Q3.java

---
+++ 31Q3.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int a = 15;
+        int b = 3;
+        double c = 3.3333333333333335;
+        int d = 45;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+
+    }
+}
+
