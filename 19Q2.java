5fd3996b20aa3ca11db0728a34ca6be5f51ff3b6
MINAMINO <causewaybar711@gmail.com>
2023-10-04 19:44:36 +0900

    Create 19Q2.java

---
+++ 19Q2.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("私は"+ "20" + "歳です。");
+
+        String year = "2020";
+        System.out.println("今年は"+ year + "年です。");
+
+        String Box = "圆周率は" + "3.14";
+        System.out.println(Box);
+
+    }
+}
+
