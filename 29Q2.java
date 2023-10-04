7b84a0cdbba665767b5aa08d375cd52248242f96
MINAMINO <causewaybar711@gmail.com>
2023-10-04 21:13:19 +0900

    Create 29Q2.java

---
+++ 29Q2.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        System.out.println((5 + 8) * 3);
+        a++;
+        System.out.println(a);
+        a--;
+        System.out.println(a);
+
+
+    }
+}
+
