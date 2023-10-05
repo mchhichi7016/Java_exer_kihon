464f3da78181604ea08dbc02761f891ac4a7910e
MINAMINO <causewaybar711@gmail.com>
2023-10-05 21:14:15 +0900

    Create 49Q4.java

---
+++ 49Q4.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 8;
+        switch (a){
+            case 1,2:
+                System.out.println("小");
+                break;
+                
+            case 3,4:
+                System.out.println("中");
+                break;
+                
+            case 5,6:
+                System.out.println("大");
+                break;
+        }
+
+    }
+
+}
+
