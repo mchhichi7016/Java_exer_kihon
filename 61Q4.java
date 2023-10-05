7c27883de2b30b0226e9f67083cbb184817fe61b
MINAMINO <causewaybar711@gmail.com>
2023-10-05 23:01:13 +0900

    Create 61Q4.java

---
+++ 61Q4.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 8;
+
+        if (a > 5 && a <10){
+            if (b == 8){
+                System.out.println("条件１に該当");
+            }
+        }if (b != 3 | b !=6 ){
+            if (b != 9){
+                System.out.println("条件2に該当");
+            }
+        }
+
+
+    }
+
+}
+
