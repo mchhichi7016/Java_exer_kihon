660de9058bd4a04d5e74faac8b134b78deb5c666
MINAMINO <causewaybar711@gmail.com>
2023-10-05 21:00:10 +0900

    Create 43Q2.java

---
+++ 43Q2.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 7;
+        if (a > 8){
+            System.out.println("aは8以上です。");
+        }
+        else {
+            System.out.println("aは8より小さいです。");
+        }
+
+    }
+
+}
+
