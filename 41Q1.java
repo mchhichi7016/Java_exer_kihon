b717bf6938bc3be9098ccca3cacd5070eef6d26c
MINAMINO <causewaybar711@gmail.com>
2023-10-05 20:54:08 +0900

    Create 41Q1.java

---
+++ 41Q1.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 12;
+        if (a > 7){
+            System.out.println("aは７より大きいです。");
+        }
+
+    }
+
+}
+
