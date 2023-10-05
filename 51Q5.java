889ac0e1c3136944db7c76d6031e51ab7d01af15
MINAMINO <causewaybar711@gmail.com>
2023-10-05 21:25:07 +0900

    Create 51Q5.java

---
+++ 51Q5.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+            int a = new  java.util.Random().nextInt(10);
+            System.out.println("a = " + a);
+            if (a % 2 ==0){
+                System.out.println("偶数");
+            }
+            else {
+                System.out.println("奇数");
+            }
+
+
+    }
+
+}
+
