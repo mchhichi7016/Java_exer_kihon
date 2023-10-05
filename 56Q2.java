c613d498d5d5f5d87e73183bd2ee6a3264dc187f
MINAMINO <causewaybar711@gmail.com>
2023-10-05 22:26:05 +0900

    Create 56Q2.java

---
+++ 56Q2.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 12;
+        boolean parent = true;
+        if (age <= 12){
+            if (!parent){
+                System.out.println("sorry");
+            }
+            else {
+                System.out.println("come on");
+            }
+        }
+        else {
+            System.out.println("come on");
+        }
+
+
+    }
+
+}
+
