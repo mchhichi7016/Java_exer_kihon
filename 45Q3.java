2d79c185865b6e37235130ca7bfe887af1d39225
MINAMINO <causewaybar711@gmail.com>
2023-10-05 21:05:10 +0900

    Create 45Q3.java

---
+++ 45Q3.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 8;
+        if (a < 7){
+            System.out.println("aは7より小さいです。");
+        }
+        else if (a == 7){
+            System.out.println("aは7です。");
+        }
+        else {
+            System.out.println("aは7より大きいです。");
+        }
+
+    }
+
+}
+
