5d789d7a55ef31eb4d863ed37437d65b4158ffd1
MINAMINO <causewaybar711@gmail.com>
2023-10-05 21:30:06 +0900

    Create 53Q6.java

---
+++ 53Q6.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int a = -5;
+
+        if (a <0){
+            a *= -1;
+        }
+        System.out.println(a);
+
+    }
+
+}
+
