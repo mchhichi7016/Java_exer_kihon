80478b1aa6ea65ba54115631388ae7f006b0d725
MINAMINO <causewaybar711@gmail.com>
2023-10-05 20:37:09 +0900

    Create 37Q6.java

---
+++ 37Q6.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 7 + 6;
+        int b = 15 - 4;
+        System.out.println(a);
+        System.out.println(b);
+        a++;
+        b--;
+        System.out.println(a);
+        System.out.println(b);
+        a = a % 3;
+        b *= 5;
+        System.out.println(a);
+        System.out.println(b);
+
+
+
+
+    }
+
+}
+
