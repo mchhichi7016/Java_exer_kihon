1a23138244aea716f2f7a5808fc14662a6c51f5a
MINAMINO <causewaybar711@gmail.com>
2023-10-05 21:37:18 +0900

    Create 54Q1.java

---
+++ 54Q1.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 7;
+        if (a == 5 || b == 7){
+            System.out.println("a: 5 or b: 7");
+        }
+        if (a == 5 && b ==7){
+            System.out.println("a: 5 and b: 7");
+        }
+
+    }
+
+}
+
