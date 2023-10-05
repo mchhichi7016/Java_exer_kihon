5619faf0330205771d99730056f0ab1f55e06509
MINAMINO <causewaybar711@gmail.com>
2023-10-05 17:57:20 +0900

    Create 35Q5.java

---
+++ 35Q5.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 8;
+        int b = 5;
+
+        System.out.println("a = " + a + "," + "b = " + b);
+        System.out.println("a + b =" + " " + (a + b));
+
+
+
+    }
+}
+
