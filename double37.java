91ed32105a5fe726cd86b7ff383dffddc8d87012
MINAMINO <causewaybar711@gmail.com>
2023-10-06 16:52:03 +0900

    Create double37.java

---
+++ double37.java
@@ -0,0 +1,27 @@
+import java.io.InterruptedIOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int number;
+        length long;//lenghは変数名ではなく、逆にすればいい。
+
+        long length;
+
+        String name,code;//複数の変数を簡単に宣言する方法
+        boolean long; //booleanタイプの値はtrueまたはfalse
+
+        double 3.7; //変数名とすることができない
+
+
+        int i = 0;
+        while (i <2){
+            i++;
+            for (int j = 0; j < 2; j++){
+                System.out.println(i * j + " ");
+            }
+        }
+
+    }
+
+}
