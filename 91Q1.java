fce3e69fde78532a8a35648de652d4cae897b2e5
MINAMINO <causewaybar711@gmail.com>
2023-10-07 10:50:37 +0900

    Create 91Q1.java

---
+++ 91Q1.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        intoroduce("Alice",12,"料理");
+    }
+
+    public static void intoroduce(String name,int age,String hobby){
+        System.out.println(name + "の興味は" + hobby + "で、年齢は" + age + "歳です。");
+    }
+}
